f04a5af79cb67c219b26a559a21458642b5788eb
Mark Nelson <marknels@hawaii.edu>
2022-02-16 13:51:36 -1000

    Add student-generated content

--- main.cpp
+++ main.cpp
@@ -3,5 +3,9 @@
 int main() {
    std::cout << "Hello, University of Hawaii, College of Engineering!" << std::endl;
    std::cout << "Lab 06d - Hello CLion - EE 205 - Spr 2022" << std::endl;
+   std::cout << std::endl;
+   std::cout << "My name is:  <<YOUR NAME HERE>>" << std::endl;
+   std::cout << "A fun fact about me is <<TELL ME HOW MUCH YOU LOVE CATS>>" << std::endl;
+
    return 0;
 }
